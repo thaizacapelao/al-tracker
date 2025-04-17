ce31fcf151370ed4074b6b0b6e5c975565c337e1
Thaiza Capelão <thaizacapelao@gmail.com>
2025-04-17 10:12:32 -0400

    att

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <main class="columns is-gapless is-multiline">
     <div class="column is-one-quarter">
-    <div class="column is-three-quarter">
-      ???
+      <BarraLateral/>
     </div>
+    <div class="column is-three-quarter">
     </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
 
 export default defineComponent({
   name: 'App',
--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -7,9 +7,23 @@
 </template>
 
 <script lang="ts">
-
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'BarraLateral'
+})
 </script>
 
 <style scoped>
-
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
 </style>
