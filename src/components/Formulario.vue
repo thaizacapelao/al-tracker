92e015380fdf22f41189fff30c36a0485b99d0bf
Thaiza Capelão <thaizacapelao@gmail.com>
2025-04-18 17:23:11 -0400

    att

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -17,15 +17,15 @@
           class="is-flex is-align-items-center is-justify-content-space-between"
         >
           <section>
-            <strong>00:00:00</strong>
+            <strong>{{ tempoDecorrido }}</strong>
           </section>
-          <button class="button">
+          <button class="button" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
             </span>
             <span>play</span>
           </button>
-          <button class="button">
+          <button class="button" @click="finalizar">
             <span class="icon">
               <i class="fas fa-stop"></i>
             </span>
@@ -42,6 +42,27 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Formulario",
+  data () {
+    return {
+      tempoEmSegundos: 0
+    }
+  },
+  computed: {
+    tempoDecorrido () : string {
+      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
+    }
+  },
+  methods: {
+    iniciar() {
+      setInterval(() => {
+        this.tempoEmSegundos += 1
+      }, 1000)
+      console.log('Iniciando')
+    },
+    finalizar() {
+      console.log('finalizando')
+    }
+  }
 });
 </script>
 
@@ -69,7 +90,7 @@ export default defineComponent({
 
 .formulario button:hover {
   background-color: rgb(214, 214, 214);
-  transition: 0.7s ease-in-out;
+  transition: background-color 0.7s ease-in-out;
 }
 
 .formulario strong {
