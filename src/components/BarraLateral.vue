540401a319fd22c1b6404ebda568b460f57cb006
Thaiza Capelão <thaizacapelao@gmail.com>
2025-04-23 17:33:15 -0400

    att

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -16,7 +16,7 @@ export default defineComponent({
 <style scoped>
 header {
   padding: 1rem;
-  background: #0d3b66;
+  background: #705043;
   width: 100%;
   height: 100vh;
 }
@@ -28,7 +28,7 @@ header {
 }
 
 img {
-  border-radius: 7%;
+  border-radius: 15%;
 }
 
 </style>
